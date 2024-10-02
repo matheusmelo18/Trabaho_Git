32ca1572e9417447abd92961e37c73a0b2830539
Bernardo Balzan <b.balzan@edu.pucrs.br>
2024-10-02 12:00:13 -0300

    Ajuste das classes Fornecedor e Item + Criação de métodos para classe Cliente

--- Fornecedor.java
+++ Fornecedor.java
@@ -6,19 +6,37 @@ public class Fornecedor {
 	
 
 	public Fornecedor(String nome, int id, Item elem){ 
-	this.nome = nome;
-	this.id = id;
-	this.item = elem;
+		this.nome = nome;
+		this.id = id;
+		this.item = elem;
 	}
 
-	public String getNome() return nome;
-	public int getId() return id;
-	public Item getItem() return item;
+	public String getNome() {
+		return nome;
+	}
 
-	public void setNome(String nome) this.nome = nome;
-	public void setId(int id) this.id = id;
-	public void setItem(Item elem) this.item = elem;
+	public int getId() {
+		return id;
+	}
 
+	public Item getItem() {
+		return item;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setItem(Item elem) {
+		this.item = elem;
+	}
+	
 	@Override
-	public String toString() return "##Fornecedor "+id+"##\n"+"Nome: "+nome+"\nNome do item: "+item.getNome()+"\nPreço do item: "+item.getPreco();
+	public String toString() {
+		return "##Fornecedor "+id+"##\n"+"Nome: "+nome+"\nNome do item: "+item.getNome()+"\nPreço do item: "+item.getPreco();
+	}
 }
